024d37598948bf0e078da700b17cd9b34f3eb771
Mohit <98217406+Fuego-19@users.noreply.github.com>
2022-06-22 13:33:53 +0530

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.app.alieninvasion {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.app.alieninvasion to javafx.fxml;
-    exports com.app.alieninvasion;
-}
